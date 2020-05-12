3aa3aa4220b45bde4354845047884712e55b17fe
Juana Garcia Seañez <jjggsscia@gmail.com>
2020-05-11 19:46:37 -0600

    Programa despliega saludo

---
+++ src/ejm_1_git/Ejm_1_Git.java
@@ -0,0 +1,19 @@
+/*
+ Ejemplo crado para subir a GitHub
+ */
+package ejm_1_git;
+
+/**
+ *
+ * @author jjggs_000
+ */
+public class Ejm_1_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola clase");
+    }
+    
+}
